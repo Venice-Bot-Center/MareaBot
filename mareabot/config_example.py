6c1cd39958a44cbd418e835f142d8ccf5edcb7b8
Fundor333 <fundor333@gmail.com>
2016-09-05 18:48:27 +0200

    Reformatting config

--- mareabot/config_example.py
+++ mareabot/config_example.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 APP_NAME = "name"
@@ -6,16 +7,17 @@ APP_DIR = os.path.dirname(os.path.realpath(__file__))
 DATABASE_NAME = "name.sqlite"
 DATABASE = 'sqlite:///%s' % os.path.join(APP_DIR, DATABASE_NAME)
 
-DEBUG = False
+DEBUG = True  # Set to False for production
 SECRET_KEY = '''Secret'''  # Used by Flask to encrypt session cookie.
 
-#Logger Config
-LOG_NAME=APP_NAME +".log"
-MAXBYTES=1000
-BACKUPCOUNT=1
+# Logger Config
+LOG_NAME = APP_NAME + ".log"
+MAXBYTES = 1000
+BACKUPCOUNT = 1
+DEBUG_LEVEL = logging.DEBUG
 
-#Twitter settings
-CONSUMER_KEY=""
-CONSUMER_SECRET=""
-ACCESS_TOKEN=""
-ACCESS_TOKEN_SECRET=""
+# Twitter settings
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
