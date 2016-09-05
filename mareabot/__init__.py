a5d4dea7d515dc28d689dd68e45cec872e03e6b9
Fundor333 <fundor333@gmail.com>
2016-09-05 18:48:58 +0200

    Cleaning the init file

--- mareabot/__init__.py
+++ mareabot/__init__.py
@@ -1,19 +1,7 @@
-import logging
-import os
-from logging.handlers import RotatingFileHandler
-
-from flask import (Flask)
-
-from config import APP_DIR,  DEBUG, SECRET_KEY, APP_NAME
-
-app = Flask(APP_NAME)
-
-handler = RotatingFileHandler(os.path.join(APP_DIR, 'marea.log'), maxBytes=10000, backupCount=1)
-handler.setLevel(logging.DEBUG)
-app.logger.addHandler(handler)
+from mareabot.api.task import set_task
+from mareabot.app import app
 
 if __name__ == '__main__':
-    from mareabot.api.task import set_task
     set_task()
     app.logger.info('Start thre app')
     app.run()
