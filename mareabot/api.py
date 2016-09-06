d4c37b76774ea82b56c5c7bf4fd91bea632feeb6
Fundor333 <fundor333@gmail.com>
2016-09-06 16:05:42 +0200

    Ceaning code

--- mareabot/api.py
+++ mareabot/api.py
@@ -2,12 +2,13 @@ import json
 
 import requests
 
-from mareabot.config import c
 from mareabot.model import PREV
 
 MAREA_API_URL = "http://dati.venezia.it/sites/default/files/dataset/opendata/previsione.json"
 
 from mareabot.task import adding_data
+
+
 def reading_api():
     datas = json.loads(requests.get(MAREA_API_URL).text)
     if not PREV.previsions:
@@ -15,6 +16,3 @@ def reading_api():
     else:
         if PREV.last != datas[0]["DATA_PREVISIONE"]:
             adding_data(datas)
-
-
-
