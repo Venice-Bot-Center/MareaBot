3e60c7d170ac494621b0f52cb59ce6d426d9020e
Matteo <fundor333@gmail.com>
2018-03-16 22:19:53 +0100

    Some cleaning

--- mareabot/api.py
+++ mareabot/api.py
@@ -1,54 +1,66 @@
 # coding=utf-8
+
 import json
 
 import requests
 from tweepy import TweepError
 
-from mareabot.model import Previsione, MemoPrev
-from mareabot.social import telegram_api, twitter_api
 
 MAREA_API_URL = "http://dati.venezia.it/sites/default/files/dataset/opendata/previsione.json"
 
+RED_CIRCLE = "🔴"
+GRAY_CIRCLE = "🔘"
+UP = "🔺"
+DOWN = "🔻"
+TIMEWATCH = "⌚"
+CALENDAR = "📆"
+
+
+class Previsione():
+    def __init__(self, previsione, estremale, tipo, valore):
+        self.data_previsione = previsione
+        self.data_estremale = estremale
+        self.tipo = tipo
+        self.valore = valore
+
+    def short_string(self, soglia):
+        date, time = self.data_estremale.split(" ")
+        date = date.split("-")[2]
+        t = time.split(":")
+        time = t[0] + ":" + t[1]
+
+        if len(self.valore) == 1:
+            self.valore = "0" + self.valore
+
+        text = ""
+        if int(self.valore) > soglia:
+            if self.tipo == "min":
+                text += DOWN
+            else:
+                text += UP
+            text += str(self.valore)+TIMEWATCH + str(time) + \
+                CALENDAR + str(date) + "\n"
+        return text
+
+    def long_string(self, soglia):
+        date, time = self.data_estremale.split(" ")
+        text = ""
+        if int(self.valore) > soglia:
+            if self.tipo == "min":
+                text += DOWN
+            else:
+                text += UP
+            text += str(self.valore) + " " + TIMEWATCH + \
+                str(time) + " " + CALENDAR + str(date) + "\n"
+        return text
+
 
 def reading_api():
-    prev = MemoPrev()
-    datas = json.loads(requests.get(MAREA_API_URL).text)
-    if prev.last != datas[0]["DATA_PREVISIONE"]:
-        adding_data(datas, prev)
-
-
-def posting(prev):
-    shorted = ""
-    estended = ""
-    flag= False
-    for s in prev.previsions:
-        if int(s.valore) >= 100:
-            flag = True
-        if (len(shorted)+len(s.short_string())>132):
-            broadcasting_text("1/2 \n"+shorted,"",False)
-            shorted ="2/2 \n"
-        shorted += s.short_string()
-        estended += s.long_string()
-    broadcasting_text(shorted, estended, flag)
-
-
-def broadcasting_text(short, long, flag):
-    if (short!=""):
-        try:
-            twitter_api.tweet_status(short)
-        except TweepError as e:
-            print (e.reason)
-    if flag == True:
-        try:
-            telegram_api.telegram_channel_send(long)
-        except Exception as e:
-            print (e.message)
-
-
-def adding_data(input_dict, prev):
-    prev.previsions = []
+    input_dict = json.loads(requests.get(MAREA_API_URL).text)
+    previsions = []
     for data in input_dict:
-        d = Previsione(data["DATA_PREVISIONE"], data["DATA_ESTREMALE"], data["TIPO_ESTREMALE"], data["VALORE"])
-        prev.previsions.append(d)
-        prev.last = input_dict[0]["DATA_PREVISIONE"]
-    posting(prev)
+        d = Previsione(data["DATA_PREVISIONE"], data["DATA_ESTREMALE"],
+                       data["TIPO_ESTREMALE"], data["VALORE"])
+        previsions.append(d)
+        last = input_dict[0]["DATA_PREVISIONE"]
+    return previsions
