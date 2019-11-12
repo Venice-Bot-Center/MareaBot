4cfeaaef3144a0bcd883ef485a349fec298ff8e7
Fundor333 <fundor333@fundor333.com>
2019-11-12 23:18:58 +0100

    Update and now marea

--- mareabot/api.py
+++ mareabot/api.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 import json
 import logging
+import re
+from bs4 import BeautifulSoup
 
 import requests
 
@@ -9,6 +11,21 @@ from mareabot.social import telegram_api
 
 logger = logging.getLogger("MareaBot")
 MAREA_API_URL = "http://dati.venezia.it/sites/default/files/dataset/opendata/previsione.json"
+
+MAREA_ISTANTANEA_API = "https://www.comune.venezia.it/it/content/centro-previsioni-e-segnalazioni-maree-beta"
+
+
+def istantanea_marea():
+    get_url = requests.get(MAREA_ISTANTANEA_API)
+    get_text = get_url.text
+    soup = BeautifulSoup(get_text, "html.parser")
+
+    company = soup.findAll('b', class_='text-marea-line5')[0].text
+    try:
+        number = int(company.split('cm')[0].split("+ ")[1])
+    except:
+        number = int(company.split('cm')[0].split("- ")[1])
+    print(number)
 
 
 def reading_api():
@@ -23,6 +40,8 @@ def posting(maximum, db_istance, hight=94):
     if int(maximum) >= hight:
         for s in db_istance.prevision:
             estended += s.long_string(hight)
+        if istantanea_marea>= 110:
+            estended+= "Ultima misurazione "+ str(istantanea_marea())+ " cm"
     try:
         if db_istance.message is not None:
             telegram_api.telegram_channel_delete_message(db_istance.message)
@@ -45,3 +64,4 @@ def adding_data(input_dict, db_istance):
         db_istance.prevision.append(d)
         db_istance.last = input_dict[0]["DATA_PREVISIONE"]
     posting(maximum=maximum, db_istance=db_istance)
+
