09355dc20ad089a4e9fb3349519aba93e8c0803f
Fundor333 <fundor333@fundor333.com>
2019-12-24 08:23:11 +0100

    Messaggi actv

--- mareabot/api/actv.py
+++ mareabot/api/actv.py
@@ -1,46 +1,30 @@
 H95 = """
-*Linea 2* limitata alla tratta San Zaccaria "B"- Tronchetto via Canale della Giudecca e San Marco Giardinetti- P.le Roma via Canal Grande.
+*Linea 2* limitata da San Zaccaria a Tronchetto e da San Marco Giardinetti a P.le Roma.
 
-*Linee 4.1 e 4.2* limitate alla tratta San Zaccaria "C" e "D"- Ospedale - F.te Nove - Murano - Tre Archi e viceversa con omissione del servizio San Zaccaria-Giudecca - P.le Roma - Ferrovia - Tre Archi e viceversa. Servizio spola tramite autobus sostitutivo tra P.le Roma e Santa Marta e servizio spola tramite mezzo acqueo tra Santa Marta e Sacca Fisola.
+*Linee 4.1 e 4.2* limitate alla tratta San Zaccaria - F.te Nove - Murano - Tre Archi con omissione San Zaccaria - Giudecca - P.le Roma - Tre Archi. Servizio spola bus tra P.le Roma e S. Marta e servizio spola motoscafo tra S. Marta e Sacca Fisola.
 
-*Linea 5.1* limitata alla tratta Lido S.M.E. "A" - Ospedale - F.te Nove - Tre Archi e viceversa
+*Linea 5.1* limitata alla tratta Lido S.M.E. "A" - Ospedale - F.te Nove - Tre Archi
 
-*Linea 5.2* dirottata via Canal Grande e limitata alla tratta P.le Roma "D"- Lido S.M.E. "B" e viceversa
-
-*Linea 6* dirottata via Canal Grande e limitata alla tratta P.le Roma "D" - Lido S.M.E. "B" e viceversa
-
-*NB*: a Ferrovia le linee _5.1/5.2_ fermano all'approdo "C" in direzione P.le Roma e all’approdo "D" in direzione Lido.
+*Linea 5.2 e 6* dirottata via Canal Grande e limitata P.le Roma- Lido S.M.E.
 
 *Linea 3 SOSPESA*
 
 Nella fascia oraria notturna, dalle 20.00 alle 7.00, gli approdi di Murano Colonna “A” e “B” vengono sospese.
 """
-H105 = (
-    H95
-    + """
 
-Si sospende *Rialto “B”* causa pendenza passerella.
-"""
-)
 H120 = (
-    H105
+    H95
     + """
 
 Si sospende Zattere direzione San Marco-Lido per assenza passerelle di collegamento all’impianto.
 """
 )
-H125 = (
+
+H130 = (
     H120
     + """
 
-Non sono più agibili le passerelle di entrata agli impianti e al punto vendita di _Rialto Linea 2 (approdi "C" e "D")_, viene mantenuta l’accessibilità dell’impianto dalle uscite sino a 140 cm.
-"""
-)
-H130 = (
-    H125
-    + """
-
-Viene interdetto il transito sotto il ponte Vivarini di Murano e pertanto è sospeso il collegamento delle _linee 4.1/4.2_ nel tratto Colonna–Venier (Via Serenella).  Questo piano dei servizi è mantenuto sino all’altezza marea + 140 cm
+E' sospeso il collegamento delle _linee 4.1/4.2_ nel tratto Colonna–Venier.
 """
 )
 H140 = """
@@ -53,12 +37,8 @@ def get_actv(hight: int) -> (str,int) :
         return H140,140
     elif hight >= 130:
         return H130,130
-    elif hight >= 125:
-        return H125,125
     elif hight >= 120:
         return H120,120
-    elif hight >= 105:
-        return H105,105
     elif hight >= 95:
         return H95,95
     return "",0
