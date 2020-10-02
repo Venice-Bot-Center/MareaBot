9f59bc96bcf2d9072da527767277a59ffc032f40
Matteo <matteo.scarpa@alilaguna.it>
2020-10-02 13:51:37 +0200

    More stuff into api

--- mareabot/social/telegram_api.py
+++ mareabot/social/telegram_api.py
@@ -10,17 +10,20 @@ CHANNEL = os.environ.get("TELEGRAM_CHANNEL", "")
 def telegram_send(text: str, user: str):
     bot = telegram.Bot(TOKEN)
     bot.send_message(chat_id=user, text=text, parse_mode=telegram.ParseMode.MARKDOWN)
+    bot.delete_webhook()
 
 
 def telegram_channel_send(text: str) -> (Message, bool):
     bot = telegram.Bot(TOKEN)
     try:
-        return (
+        out =  (
             bot.send_message(
                 chat_id=CHANNEL, text=text, parse_mode=telegram.ParseMode.MARKDOWN
             ),
             True,
         )
+        bot.delete_webhook()
+        return out
     except telegram.TelegramError:
         return None, False
 
@@ -28,6 +31,8 @@ def telegram_channel_send(text: str) -> (Message, bool):
 def telegram_channel_delete_message(message_id: str, chat: str = CHANNEL) -> bool:
     bot = telegram.Bot(TOKEN)
     try:
-        return bot.delete_message(chat_id=chat, message_id=message_id)
+        out= bot.delete_message(chat_id=chat, message_id=message_id)
+        bot.delete_webhook()
+        return out
     except telegram.TelegramError:
         return False
